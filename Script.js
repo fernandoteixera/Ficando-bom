fda65b830be4eb566b4674338824dbe8de8c93e3
fernandoteixera <164718882+fernandoteixera@users.noreply.github.com>
2024-09-04 16:34:22 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,20 @@
+const noButton = document.getElementById('no-button');
+
+function getRandomPosition() {
+    const x = Math.floor(Math.random() * (window.innerWidth - noButton.offsetWidth));
+    const y = Math.floor(Math.random() * (window.innerHeight - noButton.offsetHeight));
+    return { x, y };
+}
+
+function moveButton() {
+    const { x, y } = getRandomPosition();
+    noButton.style.position = 'absolute';
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+}
+
+noButton.addEventListener('mouseover', moveButton);
+
+function redirectToSite() {
+    window.location.href = "https://www.instagram.com/fer.devslop/"; // Substitua pelo URL desejado
+}
